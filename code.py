9c3981348a5ce03ef86c23d240cd35ee6697d184
Lloyd Najac <lloyd.najac@stu.ocsb.ca>
2023-01-10 18:51:04 +0000

    Added code.py

---
+++ code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created by: Lloyd Najac
+# Created on: Jan 2023
+
+#import ugame
+#import stage
+
+
+def game_scene():
+    # This function is the main game scene
+    print("\n\n\n")
+    print("Hello, Lloyd!")
+
+    # repeat forever game loop
+    while True:
+        pass  # A placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
